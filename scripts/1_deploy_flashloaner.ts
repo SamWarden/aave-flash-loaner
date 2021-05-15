20ee02f0bcc3508901dbee1da4a353e2362cbaae
SamWarden <SamWardenSad@gmail.com>
2021-05-15 11:43:41 +0300

    Config 1_deploy_flashloaner.ts

--- scripts/1_deploy_flashloaner.ts
+++ scripts/1_deploy_flashloaner.ts
@@ -1,22 +1,22 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 import { Contract, ContractFactory } from "ethers";
 
 /*
 
-This script deploys FlashSwap on mainnet or testnet
-If you want to use this FlashSwap - deploy it via: npx hardhat run scripts/1_deploy_flashswap.ts --network YOUR NETWORK NAME HERE
+This script deploys FlashLoaner on mainnet or testnet
+If you want to use this FlashLoaner - deploy it via: npx hardhat run scripts/1_deploy_flashloaner.ts --network YOUR NETWORK NAME HERE
 
 */
 
 // It is constants for default networks
+const AAVE_ADDRESSES_PROVIDER: string = network.name == "mainnet" ? "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5" : "0x88757f2f99175387ab4c6a4b3067c77a695b0349";
 const UNISWAP_ROUTER: string = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-const UNISWAP_FACTORY: string = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
 
 async function main(): Promise<void> {
-  const FlashSwap: ContractFactory = await ethers.getContractFactory("FlashSwap");
-  const flashSwap: Contract = await FlashSwap.deploy(UNISWAP_FACTORY, UNISWAP_ROUTER);
-  await flashSwap.deployed();
-  console.log("FlashSwap deployed successfully. Address:", flashSwap.address);
+  const FlashLoaner: ContractFactory = await ethers.getContractFactory("FlashLoaner");
+  const flashLoaner: Contract = await FlashLoaner.deploy(AAVE_ADDRESSES_PROVIDER, UNISWAP_ROUTER);
+  await flashLoaner.deployed();
+  console.log("FlashLoaner deployed successfully. Address:", flashLoaner.address);
 }
 
 main()
